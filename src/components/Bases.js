4cfd4393047db706e8bddd4cc25660694e12c9af
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-08-28 11:18:01 +0530

    made components directory

--- src/components/Bases.js
+++ src/components/Bases.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 import Base from './Base.js';
 
 class Bases extends React.Component {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Bases from './Bases';
+import Bases from './components/Bases';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
